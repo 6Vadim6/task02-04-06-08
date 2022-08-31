3940d93a2a45fddc6366127bbc698568649bf78a
6Vadim6 <112010476+6Vadim6@users.noreply.github.com>
2022-08-31 23:43:08 +0500

    Добавил .gitignore

---
+++ task0/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на себя)
+// (число умноженное на само себя).
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number*number;
+Console.WriteLine($"Квадрат числа {number} равен = {sqr}");
+
+int sqr1 = Convert.ToInt32(Math.Pow(number,2));
+Console.WriteLine($"Квадрат числа {number} равен = {sqr1}");
