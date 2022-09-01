0bdd8452f403759cf3b1fc979ba1a17a5a0eb9bd
6Vadim6 <112010476+6Vadim6@users.noreply.github.com>
2022-09-01 19:56:43 +0500

    task04

--- task02/Program.cs
+++ task02/Program.cs
@@ -2,45 +2,12 @@
 // 3 -> среда
 // 5 -> пятница
 
-Console.Write("Введите номер дня недели: ");
-string day = Console.ReadLine();
-
-if (day == "1")
-{
-    Console.WriteLine("Понедельник");
+Console.WriteLine("Введите число от 1 до 7:");
+int a = int.Parse(Console.ReadLine());
+string[] weekDays = new string[] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+if (a > 0 && a < 8) {
+	Console.WriteLine(weekDays[a-1]);
 }
-
-else if (day == "2")
-{
-    Console.WriteLine("Вторник");
-}
-
-else if (day == "3")
-{
-    Console.WriteLine("Среда");
-}
-
-else if (day == "4")
-{
-    Console.WriteLine("Четверг");
-}
-
-else if (day == "5")
-{
-    Console.WriteLine("Пятница");
-}
-
-else if (day == "6")
-{
-    Console.WriteLine("Суббота");
-}
-
-else if (day == "7")
-{
-    Console.WriteLine("Воскресенье");
-}
-
-else
-{
-    Console.WriteLine("Такого не бывает!");
+else {
+	Console.WriteLine("Не путай меня");
 }
