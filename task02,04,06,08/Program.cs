161dbca4f21e312449646f3464f0b934a24497ab
6Vadim6 <112010476+6Vadim6@users.noreply.github.com>
2022-09-02 22:31:15 +0500

    task6 пока не решил

--- task02,04,06,08/Program.cs
+++ task02,04,06,08/Program.cs
@@ -1,10 +1,7 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
-Console.Write("Введите два числа: ");
-string[] num = Console.ReadLine().Split(' ');
-int a = int.Parse(num[0]);
-int b = int.Parse(num[1]);
-if (a > b) Console.WriteLine($"{a}");
-else Console.WriteLine($"{b}");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+int b = a/2;
