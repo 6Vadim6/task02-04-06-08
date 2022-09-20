e934b5e8bc94b8515b32ff0067d7cec38476ef16
6Vadim6 <112010476+6Vadim6@users.noreply.github.com>
2022-09-20 20:27:31 +0500

    seminar4 task26,28

---
+++ seminar4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+Console.WriteLine("Enter number: ");
+int number1 = int.Parse(Console.ReadLine());
+int number = Math.Abs(number1);
+
+int Getcount(int number)
+{
+    int count = 0;
+    while (number > 10)
+    {
+        number = number / 10;
+        count++;
+    }
+    return count+1;
+}
+
+Console.WriteLine($"{number1} -> {Getcount(number)}");
---
+++ seminar4/task28/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Clear();
+Console.WriteLine("Enter number N: ");
+int number = int.Parse(Console.ReadLine());
+
+int Factorial(int num)
+{
+    int count = num;
+    while (count > 1)
+    {
+        count--;
+        num *= count;
+        Console.WriteLine(num);
+    }
+    return num;
+}
+
+Console.WriteLine($"{number} -> {Factorial()}");
